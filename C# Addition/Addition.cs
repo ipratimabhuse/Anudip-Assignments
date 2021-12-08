5d0e114fbe21ad6996fc66415f2c610d542f3f1e
ipratimabhuse <95672616+ipratimabhuse@users.noreply.github.com>
2021-12-08 09:45:45 +0530

    Add files via upload

---
+++ C# Addition/Addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Addition
+{
+    static void Main(string[] args)
+    {
+        int x, y;
+        Console.Write("\nInput the First Number : ");
+        x = int.Parse(Console.ReadLine());
+        Console.Write("\nInput the Second Number : ");
+        y = int.Parse(Console.ReadLine());
+        int result = x + y;
+        Console.Write(result);
+        Console.Read();
+
+    }
+   
+}
---
+++ C# Swap/Swap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Swap
+{
+    static void Main(string[] args)
+    {
+        int x, y, temp;
+        Console.Write("\nInput the First Number : ");
+        x = int.Parse(Console.ReadLine());
+        Console.Write("\nInput the Second Number : ");
+        y = int.Parse(Console.ReadLine());
+        temp = x;
+        x = y;
+        y = temp;
+        Console.Write("After Swap :");
+        Console.Write("\nInput the First Number :" +x);
+        Console.Write("\nInput the Second Number :" +y);
+        Console.Read();
+    }
+}
+
