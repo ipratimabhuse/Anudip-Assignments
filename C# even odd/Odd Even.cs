2e12e235800ec0c9b59be720dbdcb7f1e1f7e104
ipratimabhuse <95672616+ipratimabhuse@users.noreply.github.com>
2021-12-08 14:59:02 +0530

    Add files via upload

---
+++ C# even odd/Odd Even.cs
@@ -0,0 +1,19 @@
+﻿using System;
+    class Even_Odd
+{
+    static void Main(string[] args)
+    {
+        int num;
+        Console.Write("\nEnter the number : ");
+        num = int.Parse(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("It is even number");
+        }
+        else
+        {
+            Console.WriteLine("It is odd number");
+        }
+    }
+
+}
