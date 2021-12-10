e0219eb0ffb9fd148cbd2d67041e1a44b918a11c
ipratimabhuse <95672616+ipratimabhuse@users.noreply.github.com>
2021-12-10 13:11:15 +0530

    Add files via upload

---
+++ C# Difference/Absolute diffence/Difference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Difference
+{
+    class Difference
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(53));
+            Console.WriteLine(test(30));
+            Console.WriteLine(test(51));
+            Console.ReadLine();
+        }
+        static int test(int n)
+        {
+            const int x = 51;
+            if (n > x)
+            {
+                return (n - x) * 3;
+            }
+            return (x - n);
+        }
+            
+    }
+}
