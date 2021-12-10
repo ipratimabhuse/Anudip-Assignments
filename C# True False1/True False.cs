0d3e0f9e6fb9b1a11bff43bca9d9ca25c89326ba
ipratimabhuse <95672616+ipratimabhuse@users.noreply.github.com>
2021-12-10 13:18:43 +0530

    Add files via upload

---
+++ C# True False1/True False.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace True
+{
+  class True
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine(test(30, 0));
+        Console.WriteLine(test(25,5));
+        Console.WriteLine(test(20,30));
+        Console.WriteLine(test(20,25));
+        Console.ReadLine();
+    }
+    static bool test(int x, int y)
+    {
+        return x == 30 || y == 30 || (x + y == 30);
+    }
+  }
+}
---
+++ C# True False2/True False2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace True2
+{
+    class True2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(test(103));
+            Console.WriteLine(test(90));
+            Console.WriteLine(test(89));
+            Console.ReadLine();
+        }
+        static bool test(int x)
+        {
+            if (Math.Abs(x - 100) <= 10 || Math.Abs(x - 200) <= 10) 
+            return true;
+            return false;
+        }
+    }
+}
