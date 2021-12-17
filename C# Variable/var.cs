137a7ff965d2b5d768a9f9fad27208bb27474a7e
ipratimabhuse <95672616+ipratimabhuse@users.noreply.github.com>
2021-12-17 11:50:58 +0530

    Add files via upload

---
+++ C# Variable/var.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CSharpFeatures
+{
+    class ImplicitTypedExample
+    {
+        public static void Main()
+        {
+            // integer
+            var a = 10;
+            // string
+            var s = "javatpoint";
+            // array
+            var arr = new[] { 2,5,6 };
+            Console.WriteLine(a);
+            Console.WriteLine(s);
+            Console.WriteLine(arr[2]);
+        }
+    }
+}
