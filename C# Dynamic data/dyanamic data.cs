8121ab460333aa30e40fa9e466d258b5a0b5fe54
ipratimabhuse <95672616+ipratimabhuse@users.noreply.github.com>
2021-12-20 10:41:43 +0530

    Add files via upload

---
+++ C# Dynamic data/dyanamic data.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    class Demo
+    {
+        dynamic Data = 12;
+        public int Method(int P, int Q)
+        {
+            return (P + Q) * Data;
+        }
+    }
+    class dyanamic
+    {
+        static void Main(string[] args)
+        {
+            Demo Obj = new Demo();
+            dynamic value1 = 10;
+            dynamic value2 = 11;
+            dynamic Str = "Your Result Is: ";
+            Console.WriteLine(Str + Obj.Method(value1, value2));
+            Console.ReadLine();
+        }
+    }
