93570cc06ee18d5879505bc7dd2340ece2ca9574
ipratimabhuse <95672616+ipratimabhuse@users.noreply.github.com>
2021-12-21 10:58:39 +0530

    Add files via upload

---
+++ C# CharArray/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Test
+{
+    static void Main(string[] args)
+    {
+        char[] charArray = { 'a', 'b', 'c', 'd', 'e', 'f' };
+        String s = new string(charArray);
+
+        Console.WriteLine(s);
+    }
+}
